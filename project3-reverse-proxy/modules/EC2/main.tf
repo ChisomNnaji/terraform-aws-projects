eecaee780197cefcf2eede7e93242b1d9887b7d0
devops4sure <chisomnnaji51@gmail.com>
2025-11-25 21:38:30 +0100

    reverse proxy

--- project3-reverse-proxy/modules/EC2/main.tf
+++ project3-reverse-proxy/modules/EC2/main.tf
@@ -27,14 +27,55 @@ resource "aws_instance" "nginx" {
     Name = "nginx-server"
   }
 
-  user_data = <<-EOF
+user_data = <<-EOF
             #!/bin/bash
+            # Update system packages
             dnf update -y
-            dnf install -y nginx
+
+            # Install Nginx and unzip tool
+            dnf install -y nginx unzip
+
+            # Enable and start Nginx
             systemctl enable nginx
             systemctl start nginx
-            echo "<h1>Hello from Terraform EC2!</h1>" > /usr/share/nginx/html/index.html
+
+            # Navigate to web root
+            cd /usr/share/nginx/html
+
+            # Download the Nexus Brew template
+            curl -LO https://www.tooplate.com/zip-templates/2146_nexus_brew.zip
+
+            # Unzip the template to this directory
+            unzip 2146_nexus_brew.zip
+
+            # Move all files out of the unzipped folder to web root
+            mv 2146_nexus_brew/* ./
+
+            # Clean up
+            rm -rf 2146_nexus_brew 2146_nexus_brew.zip
+
+            # Optional: remove default index if exists
+            rm -f index.html
+
+            # Reload Nginx to reflect changes
+            systemctl reload nginx
+
+            # Configure Nginx as a reverse proxy (if you have a backend)
+            cat > /etc/nginx/conf.d/reverse-proxy.conf <<EOL
+            server {
+                listen 80;
+                location / {
+                    proxy_pass http://BACKEND_PRIVATE_IP;
+                    proxy_set_header Host \$host;
+                    proxy_set_header X-Real-IP \$remote_addr;
+                    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+                }
+            }
+            EOL
+
+            systemctl reload nginx
             EOF
+
 }
 
 
