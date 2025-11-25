577172e5f5812528320e77bde3d9627eccaf15f7
devops4sure <chisomnnaji51@gmail.com>
2025-11-25 19:41:44 +0100

    template update

--- project4-load-balancer/modules/EC2/main.tf
+++ project4-load-balancer/modules/EC2/main.tf
@@ -4,17 +4,20 @@ resource "aws_instance" "web" {
   subnet_id              = var.public_subnet_id
   vpc_security_group_ids = [var.security_group_id]
 
-  user_data = <<-EOF
-              #!/bin/bash
-              dnf update -y
-              dnf install -y nginx unzip
-              systemctl enable nginx
-              systemctl start nginx
-              cd /usr/share/nginx/html
-              curl -O https://www.tooplate.com/zip-templates/2145_eco_lume.zip
-              unzip 2145_eco_lume.zip
-              rm -f 2145_eco_lume.zip
-              EOF
+user_data = <<-EOF
+            #!/bin/bash
+            dnf update -y
+            dnf install -y nginx unzip
+            systemctl enable nginx
+            systemctl start nginx
+            cd /usr/share/nginx/html
+            rm -f index.html
+            curl -O https://www.tooplate.com/zip-templates/2145_eco_lume.zip
+            unzip 2145_eco_lume.zip
+            mv 2145_eco_lume/* ./
+            rm -rf 2145_eco_lume 2145_eco_lume.zip
+            EOF
+
 
   tags = {
     Name = "eco-lume-server"
